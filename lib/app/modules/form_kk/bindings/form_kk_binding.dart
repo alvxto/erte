1e6b6be259eb36bc97c6217217312ff8159d4c5d
Alvito Sandi R <alvxtosanz@gmail.com>
2022-08-05 08:07:59 +0700

    binding kk

--- lib/app/modules/form_kk/bindings/form_kk_binding.dart
+++ lib/app/modules/form_kk/bindings/form_kk_binding.dart
@@ -1,3 +1,4 @@
+import 'package:erte/app/modules/form_kk/controllers/form_anggota_controller.dart';
 import 'package:get/get.dart';
 
 import '../controllers/form_kk_controller.dart';
@@ -7,6 +8,9 @@ class FormKkBinding extends Bindings {
   void dependencies() {
     Get.lazyPut<FormKkController>(
       () => FormKkController(),
+    );
+    Get.lazyPut<FormAnggotaController>(
+      () => FormAnggotaController(),
     );
   }
 }
